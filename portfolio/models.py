6fccb74f96f1b2085c7c41a852ea75098c2c43b1
yunus emre tosun <sn.emretosun@gmail.com>
2021-02-01 01:39:05 +0300

    new commit

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=200)
-    image = models.ImageField(upload_to="porfolio/img/")
+    image = models.ImageField(upload_to="portfolio/img/")
     url = models.URLField(blank=True)
 
     def __str__(self):
